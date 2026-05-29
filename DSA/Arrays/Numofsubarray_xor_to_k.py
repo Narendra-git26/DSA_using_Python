2fce092dc7f158e3fbcf451f5d092595e4d535f3
Narndra varma Koneti <konetinarendravarma212@gmail.com>
2026-05-29 09:19:00 +0530

    number of subarray using xor to k

---
+++ DSA/Arrays/Numofsubarray_xor_to_k.py
@@ -0,0 +1,37 @@
+# Better solution
+# Time O(n^2)
+# space = O(1) constant space
+# nums = [4,2,2,6,4]
+# k = 6
+# count = 0
+# n = len(nums)
+# for i in range(0,n):
+#     xor = 0
+#     for j in range(i , n):
+#         xor = xor ^ nums[j]
+#
+#         if xor == 6:
+#             count += 1
+# print(count)
+
+
+nums = [4,2,2,6,4]
+
+k = 6
+count = 0
+hash_map = {0:1}
+xor = 0
+for i in range(0,len(nums)):
+    xor = xor ^ nums[i]
+
+    if xor == k:
+        count += 1
+    x = xor ^ k
+    if x in hash_map:
+        count += hash_map[x]
+    hash_map[xor] = hash_map.get(xor, 0) + 1
+    print(count)
+
+
+
+
